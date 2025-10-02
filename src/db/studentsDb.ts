8cc6e202dccc58be5e3085a22092293a9ce8d369
chutchikovas-rgb <chutchikovas@mer.ci.nsu.ru>
2025-10-02 12:15:57 +0700

    qwe

--- src/db/studentsDb.ts
+++ src/db/studentsDb.ts
@@ -7,29 +7,29 @@ sqlite3.verbose();
 export const getStudentsDb = async (): Promise<StudentInterface[]> => {
   const db = new sqlite3.Database(process.env.DB ?? './db/vki-web.db');
 
-  //const students = await new Promise((resolve, reject) => {
-    //const sql = 'SELECT * FROM class';
-    //db.all(sql, [], (err, rows) => {
-      //if (err) {
-        //reject(err);
-        //db.close();
-     //   return;
-      //}
-      //resolve(rows);
-    //  db.close();
-  //  });
- // });
+  const student = await new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM student';
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+        db.close();
+       return;
+      }
+      resolve(rows);
+     db.close();
+   });
+ });
 
   // test data
-  const students: StudentInterface[] = [
-     {
-      id: 1,
-      first_name: 'lo',
-      last_name: 'lo',
-      middle_name: 'lo',
-      groupid: 2,
-     },
-   ];
+  // const students: StudentInterface[] = [
+  //    {
+  //     id: 1,
+  //     first_name: 'lo',
+  //     last_name: 'Раесае',
+  //     middle_name: 'Вайперрович',
+  //     groupid: 2,
+  //    },
+  //  ];
 
-  return students as StudentInterface[];
+  return student as StudentInterface[];
 };
